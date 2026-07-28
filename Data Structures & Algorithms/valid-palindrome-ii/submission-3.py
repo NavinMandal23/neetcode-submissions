4c71659627bff5de6eda33143450b25966a226a3
NavinMandal23 <navinmandal.pvt@gmail.com>
2026-07-28 14:23:41 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        deleted = None
+        L, R = 0, len(s) - 1
+        while L <= R:
+            if s[L] != s[R]:
+                deleted = True
+                break
+            L += 1
+            R -= 1
+        print(s[:L], s[L+1:]) # left deleted part
+        print(s[:R], s[R+1:]) # right deleted part
+
+        if deleted == True:
+            ldp = s[:L] + s[L+1:]
+            rdp = s[:R] + s[R+1:]
+            return ldp == ldp[::-1] or rdp == rdp[::-1]
+        else:
+            return s == s[::-1]
+        
